3dcd18defb3b5f973922938b1eb7829bd551335c
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-05-27 15:00:00 +0600

    latest product section updated

--- src/pages/Home/Product/Product.js
+++ src/pages/Home/Product/Product.js
@@ -7,10 +7,13 @@ const Product = () => {
                 <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" class="rounded-xl" />
             </figure>
             <div class="card-body items-center text-center">
-                <h2 class="card-title">Shoes!</h2>
+                <h2 class="card-title">Product Name</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
+                <p><small>Minimum Order Quantity: 100</small></p>
+                <p><small>Available Quantity: 1000</small></p>
+                <p><small>Price: $500</small></p>
                 <div class="card-actions">
-                    <button class="btn btn-primary">Buy Now</button>
+                    <button class="btn btn-primary uppercase">purchase now</button>
                 </div>
             </div>
         </div>
