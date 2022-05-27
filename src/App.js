c891ce7897eaa1b666e8892062f0c493883740fd
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-05-27 15:43:20 +0600

    complete setup protected route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navber from './pages/Shared/Navber/Navber';
+import Navbar from "./pages/Shared/Navbar";
 import Home from "./pages/Home/Home/Home";
-import Products from "./pages/Home/Products/Products";
+import Footer from "./pages/Shared/Footer";
+import Login from "./pages/LogIn/Login";
+import SignUp from "./pages/LogIn/SignUp";
+import { Route, Routes } from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import Products from "./pages/Home/Products/Products";
+import RequireAuth from "./pages/LogIn/RequireAuth";
+import Purchase from "./pages/Purchase/Purchase";
 
 function App() {
   return (
-    <>
-    <Navber></Navber>
+    <div className="">
+      <Navbar></Navbar>
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
-        <Route path="/products" element={<Products></Products>}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<PageNotFound />}></Route>
+        <Route path="/products" element={<Products />}></Route>
+        <Route
+          path="/purchase"
+          element={
+            <RequireAuth>
+              <Purchase />
+            </RequireAuth>
+          }
+        ></Route>
       </Routes>
-    </>
+      <Footer></Footer>
+    </div>
   );
 }
 
